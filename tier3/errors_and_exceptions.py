083423a4fe9d4c8ede72aeae072907bda5fad298
otumian <popecan1000@gmail.com>
2019-08-23 21:53:25 +0000

    errors and exceptions - initial commit

---
+++ tier3/errors_and_exceptions.py
@@ -0,0 +1,4 @@
+# errors and exceptions
+# syntax error = parsing error (complaints - they come from a misuse of a token)
+# exception error = runtime error (they are detected when the script is executed 
+# - may come from an error in the logic, perhaps)
