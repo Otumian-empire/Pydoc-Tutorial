848b1c1358c8aa68312b5c5448909d3143b3d93b
otumian <popecan1000@gmail.com>
2019-08-23 11:18:29 +0000

    wrote and read using json's dump and load

--- tier3/write_read_json.py
+++ tier3/write_read_json.py
@@ -1,11 +1,29 @@
 # write and read with json
 from json import dumps, dump, load, loads
-print(repr(dumps(['hello', 'world'])))  # for viewing the json repr
+# print(repr(dumps(['hello', 'world'])))  # for viewing the json repr
 
-# dump - write into a file using json
-with open('tier3/new_json_file.txt', 'w+') as new_json_file_obj:
-    dump("hello world json", new_json_file_obj)
+# # dump - write into a file using json
+# with open('tier3/new_json_file.txt', 'w+') as new_json_file_obj:
+#     dump("hello world json", new_json_file_obj)
 
-# load - read from a file using json
-with open('tier3/new_json_file.txt', 'r+') as old_json_file:
-    print(loads(old_json_file))
+# # load - read from a file using json
+# with open('tier3/new_json_file.txt', 'r+') as old_json_file:
+#     print(load(old_json_file))
+
+
+my_dict = {
+    'name': "john doe",
+    'age': 23,
+    'hobby': "programming",
+    'profession': "web developer"
+}
+
+with open('tier3/dict_json_file.txt', 'w+') as old_json_file:
+    dump(my_dict, old_json_file)
+
+with open('tier3/dict_json_file.txt', 'r+') as old_json_file:
+    my_dict = load(old_json_file)
+    for key, value in my_dict.items():
+        print(f"{key:10} ==> {value}")
+
+# we'd look into deep into json and pickle
