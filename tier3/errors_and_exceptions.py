98a80bb4c101c52b9df151247eb0d965a66f5fe0
otumian <popecan1000@gmail.com>
2019-08-24 02:06:10 +0000

    added a new file operatorerror.py for OperatorError class and moved the OperatorError class into it

--- tier3/errors_and_exceptions.py
+++ tier3/errors_and_exceptions.py
@@ -78,24 +78,7 @@ ZeroDivisionError: division by zero
 
 
 
-class OperatorError(BaseException):
-    """ exception handler for operator error """
 
-    def __init__(self, operator):
-        self.operator = operator
-
-    def raise_OperatorError(self):
-        """ raises an OperatorError (this is likely to be a SynthaxError as it is called when 
-        the operator is nor recognised)"""
-
-        operators = ['%', '*', '**', '-', '+', '/', '//']
-
-        if self.operator not in operators:
-            print(
-                f"OperatorError: {repr(self.operator)}, is not known, use any of {repr(operators)}")
-            return False
-
-        return True
 
 
 class Calculator:
