e0134c7e8ea738b92e87246051443c9ab5091191
otumian <popecan1000@gmail.com>
2019-08-24 02:02:48 +0000

    added a new file operanderror.py for OperandError class and moved the OperandError class into it

--- calculatorerror.py
+++ calculatorerror.py
@@ -1,5 +1,5 @@
 # user defined-exceptions
-# 
+# an exception class, that will serve as the base file for calculations
 class CalculatorError(Exception):
     """ Base class for my exception handlers for the calculator class """
     pass
