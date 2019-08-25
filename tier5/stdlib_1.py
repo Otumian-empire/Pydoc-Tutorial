9fadd5b828cd624c41278adc279b710e072525c3
otumian <popecan1000@gmail.com>
2019-08-25 06:45:00 +0000

    tier5/stdlib_1.py- shutil and glob

--- tier5/stdlib_1.py
+++ tier5/stdlib_1.py
@@ -1,20 +1,30 @@
 # Brief Tour of the Standard Library
-# os
-import os
 
-print(f"The current working directory: {os.getcwd()}")
-os.chdir('tier5')
-print(f"Changed directory into directory: {os.getcwd()}")
-os.chdir('../')
-print(f"Now the current working directory: {os.getcwd()}")
+import glob  # list of a wildcard search
+import shutil  # file and directory system interface
+import os  # operating system interface
 
-# ls, create a file with touch, add text and echo the text from the file
-# ls again
-os.system('ls')  # execute cmds in a subshell
-os.system("touch ossystem")
-os.system("echo 'haha' >> ossystem")
-os.system("cat ossystem")
-os.system('ls')
+# # working briefly with the os module
+# print(f"The current working directory: {os.getcwd()}")
+# os.chdir('tier5')
+# print(f"Changed directory into directory: {os.getcwd()}")
+# os.chdir('../')
+# print(f"Now the current working directory: {os.getcwd()}")
+
+# # ls, create a file with touch, add text and echo the text from the file
+# # ls again
+# os.system('ls')  # execute cmds in a subshell
+# os.system("touch ossystem")
+# os.system("echo 'haha' >> ossystem")
+# os.system("cat ossystem")
+# os.system('ls')
 
 # os as open() which there is open also one for opening of files
 # avoid `import os *`
+
+
+# # working briefly with the shutil module
+# print(shutil.copyfile('ossystem', 'helloworld'))
+
+# working briefly with the glob module
+# print(glob.glob('../*'))  # read all that ends with .py
