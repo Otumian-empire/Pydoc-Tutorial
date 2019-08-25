b8f7b3fe90e381346e5590d5f36993fbdb596608
otumian <popecan1000@gmail.com>
2019-08-25 02:01:23 +0000

    tier4/iterations.py

---
+++ tier4/iterations.py
@@ -0,0 +1,13 @@
+# iterations is just like loops
+mylist = [1, 2, 3, 4]
+# for i in mylist:
+#     print(i)
+
+# note that for actually calls the iter() and __next__() -> next()
+# iterthe 2nd arg tells iter to return the next item
+it_mylist = iter(mylist)
+# print(next(it_mylist))
+# print(next(it_mylist))
+# print(next(it_mylist))
+# print(next(it_mylist))
+
