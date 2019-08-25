4f9cbd878561523c9cc99b6bba3e6741e3ed97f3
otumian <popecan1000@gmail.com>
2019-08-25 13:33:47 +0000

    tier5/stdlib_1.py - doctest abd unittest for quality control

--- tier5/stdlib_1.py
+++ tier5/stdlib_1.py
@@ -1,5 +1,10 @@
 # Brief Tour of the Standard Library
 
+import doctest
+import unittest
+import timeit
+import zlib
+import datetime
 from urllib.request import urlopen
 import statistics
 import random
@@ -55,3 +60,60 @@ import os  # operating system interface
 #         if '<a' in line:
 #             line = line[line.index('<a'):]
 #             print(line, file=open('ossystem.txt', 'a+'))
+
+
+# # working briefly with the datetime module
+# print(datetime.date(1996, 12, 2))
+# print(datetime.time(23, 30, 12))
+# print(datetime.datetime(2012, 12, 2, 2, 3, 4))
+# print(datetime.datetime.now())
+# print(datetime.date.today().strftime(
+#     "%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
+
+# # working briefly with the data compression module - zlib
+# data = b"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita cupiditate nam perferendis perspiciatis quasi, commodi quae deleniti rerum, provident suscipit possimus, eligendi repudiandae soluta repellendus neque eaque dolorem. Placeat, id!"
+
+# data_size = len(data)
+
+# compressed_data = zlib.compress(data)
+# compressed_data_size = len(compressed_data)
+
+# decompressed_date = zlib.decompress(compressed_data)
+# decompressed_date_size = len(decompressed_date)
+
+# print(f"date size: {data_size}\ncompressed data size: {compressed_data_size}\ndecompressed data size: {decompressed_date_size}")
+
+
+# working briefly with the timeit module
+# print(timeit.Timer('a += 1', 'a = 1').repeat(2))
+# print(timeit.Timer('a = a + 1', 'a = 1').repeat(2))
+# print(timeit.Timer('a ** 2', 'a=2').repeat(2))
+# print(timeit.Timer('a * a', 'a=2').repeat(2))
+# print(timeit.Timer('math.pow(a, 2)', 'a=2').repeat(2)) # name error
+# print(timeit.Timer('n-(d * q)', 'n=5; d=3;q = n//d').repeat(10))
+# print(timeit.Timer('n % d', 'n=5; d=3').repeat(10))
+
+# quality control
+# doctest module
+def say_hi():
+    """ returns a hi 
+
+    >>> say_hi()
+    'hi'
+    """
+    return 'hi'
+
+
+doctest.testmod()
+
+# unittest
+
+
+class SayHiTest(unittest.TestCase):
+
+    def test_say_hi_(self):
+        self.assertTrue(say_hi(), "must return a value")
+        self.assertEqual(say_hi(), 'hi', 'say_hi is supposed to return hi')
+
+
+unittest.main()
