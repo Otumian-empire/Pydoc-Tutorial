93b79fb05f4ccdaf2786a7bd85caad6a68ef6c23
otumian <popecan1000@gmail.com>
2019-08-25 07:36:32 +0000

    tier5/stdlib_1.py - added urllib

--- tier5/stdlib_1.py
+++ tier5/stdlib_1.py
@@ -1,5 +1,6 @@
 # Brief Tour of the Standard Library
 
+from urllib.request import urlopen
 import statistics
 import random
 import math
@@ -30,19 +31,27 @@ import os  # operating system interface
 # # working briefly with the shutil module
 # print(shutil.copyfile('ossystem', 'helloworld'))
 
-# working briefly with the glob module
+# # working briefly with the glob module
 # print(glob.glob('../*'))  # read all that ends with .py
 
-# working briefly with the sys module
+# # working briefly with the sys module
 # print(sys.argv)
 # print(sys.stderr.write("error"))
 # sys.exit()
 
-# working briefly with the math module
-print(f"The area of a circle of 3cm is {math.pi * math.pow(3, 2):.2f}cm^2")
+# # working briefly with the math module
+# print(f"The area of a circle of 3cm is {math.pi * math.pow(3, 2):.2f}cm^2")
 
-# working briefly with the random module
-print(random.randint(1, 10))
+# # working briefly with the random module
+# print(random.randint(1, 10))
 
 # working briefly with the statistics module
-print("The mean of [2,3,5]", statistics.mean([2, 3, 5]))
+# print("The mean of [2,3,5]", statistics.mean([2, 3, 5]))
+
+# # working briefly with the internet module - urllib
+# with urlopen('file:///home/otumian/Documents/Python/python-3.7.3-docs-html/tutorial/stdlib.html') as responds:
+#     for line in responds:
+#         line = line.decode('utf-8')
+#         if '<a' in line:
+#             line = line[line.index('<a'):]
+#             print(line, file=open('ossystem.txt', 'a+'))
